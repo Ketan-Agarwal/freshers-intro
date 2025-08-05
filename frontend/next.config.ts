3d539fd273cc604d12ee13663d2102dd3d392190
Ketan <ketan.agarwal2506@gmail.com>
2025-08-06 02:25:57 +0530

    fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next"
-const BACKEND = process.env.NEXT_PUBLIC_BACKEND_ORIGIN
+const BACKEND = process.env.NEXT_PUBLIC_BACKEND_URL
 const nextConfig: NextConfig = {
   images: {
     domains: ['freshers-intro-images.s3.eu-north-1.amazonaws.com','local-host-thing.s3.eu-north-1.amazonaws.com',
